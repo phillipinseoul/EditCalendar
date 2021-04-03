8337c3394881f60d6c9df8dc0d3b804601f7feb2
phillip9807@gmail.com <phillip9807@gmail.com>
2021-04-03 17:51:58 +0900

    add allDa btn

--- app/src/main/java/com/example/editcalendar/MainActivity.java
+++ app/src/main/java/com/example/editcalendar/MainActivity.java
@@ -7,13 +7,31 @@ import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText title, location, description;
     Button addEvent;
+    Switch checkAllDay;
+    Boolean allDay;
+
+
+    // Set listener for all day button
+    class allDaySwitchListener implements CompoundButton.OnCheckedChangeListener{
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                allDay = true;
+            } else {
+                allDay = false;
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +43,13 @@ public class MainActivity extends AppCompatActivity {
         location = findViewById(R.id.inputLocation);
         description = findViewById(R.id.inputDesc);
         addEvent = findViewById(R.id.addEvent);
+        checkAllDay = findViewById(R.id.checkAllDay);
 
 
+        checkAllDay.setOnCheckedChangeListener(new allDaySwitchListener());
+
+
+        // Add a new event by click addEvent button
         addEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
                     intent.putExtra(CalendarContract.Events.TITLE, title.getText().toString());
                     intent.putExtra(CalendarContract.Events.DESCRIPTION, description.getText().toString());
                     intent.putExtra(CalendarContract.Events.EVENT_LOCATION, description.getText().toString());
-                    intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+                    // intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, allDay);
                     intent.putExtra(Intent.EXTRA_EMAIL, "test@gmail.com, test2@gmail.com, test3@gmail.com");
 
                     if (intent.resolveActivity(getPackageManager()) != null){
