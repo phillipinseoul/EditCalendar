25e8ae52430ecf7888b1a0fc5b844a53cec25f9f
phillip9807@gmail.com <phillip9807@gmail.com>
2021-04-03 15:59:34 +0900

    init MainAcitivity

--- app/src/main/java/com/example/editcalendar/MainActivity.java
+++ app/src/main/java/com/example/editcalendar/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.editcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText title, location, description;
+    Button addEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        title = findViewById(R.id.inputTitle);
+        location = findViewById(R.id.inputLocation);
+        description = findViewById(R.id.inputDesc);
+        addEvent = findViewById(R.id.addEvent);
+
+
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If every field is filled
+                if (!title.getText().toString().isEmpty() && !location.getText().toString().isEmpty() && !description.getText().toString().isEmpty()) {
+
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.TITLE, title.getText().toString());
+                    intent.putExtra(CalendarContract.Events.DESCRIPTION, description.getText().toString());
+                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, description.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+                    intent.putExtra(Intent.EXTRA_EMAIL, "test@gmail.com, test2@gmail.com, test3@gmail.com");
+
+                    if (intent.resolveActivity(getPackageManager()) != null){
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "There is no app that can support this action", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // If there is an empty field
+                    Toast.makeText(MainActivity.this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
     }
 }
